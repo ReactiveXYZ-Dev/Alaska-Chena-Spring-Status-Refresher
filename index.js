d4dc4772a97c6886e174e0d1c045f14ebb848eb6
ReactiveXYZ-Dev <jackie@reactive.xyz>
2018-02-27 21:41:00 -0800

    Testing auto reloading

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ var mailClient = mailgun.client({
     key: "key-e593ce801035acd418bf37df27feb6e2"
 })
 
+var sendMail = function() {
+    // send email
+    mailClient.messages.create('mail.classspy.com', {
+        from: "Alaska Tour <internal@classspy.com>",
+        to: ["jackiee1998@hotmail.com"],
+        subject: "Rooms available for 3rd Mar!",
+        html: "Come be quick! <a href='https://secure.chenahotsprings.com/webres/webres.asp'>Click here to register</a> ."
+    })
+    .then(msg => console.log(msg))
+    .catch(err => console.log(err));
+}
+
 var scrape = function() {
     // create scrapper process
     var p = cp.spawn("casperjs", ["scraper.js"]);
@@ -13,24 +25,20 @@ var scrape = function() {
     // register stdout listeneres
     p.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
+
         if (data == "HAS_ROOMS\n") {
             console.log("GREAT!!!!");
-            // send email
-            mailClient.messages.create('mail.classspy.com', {
-                from: "Alaska Tour <internal@classspy.com>",
-                to: ["jackiee1998@hotmail.com"],
-                subject: "Rooms available for 3rd Mar!",
-                html: "Come be quick! <a href='https://secure.chenahotsprings.com/webres/webres.asp'>Click here to register</a> ."
-            })
-            .then(msg => console.log(msg)) 
-            .catch(err => console.log(err));
+            sendMail();
         } else if (data == "NO_ROOMS\n") {
             console.log("SHIT!!!");
         }
+
     });
+
     p.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
     });
+    
     p.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
     });
--- scraper.js
+++ scraper.js
@@ -1,44 +1,68 @@
-var casper = require('casper').create();
-
-casper.start("https://secure.chenahotsprings.com/webres/webres.asp");
-
-casper.waitForSelector('div.calendarDIV', function() {
-    this.echo("Loaded page...");
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: 'debug'
 });
 
-casper.then(function() {
-    // go to next month for arrival
-    this.click("td.calendarmmyyyy tr td:last-child input[type=submit]");
-});
+// extract cli arguments
+// dates in form YYYY-MM-DD
+// TODO: convert them into actions
+var arrDate = casper.cli.get("arrive-date");
+var deptDate = casper.cli.get("dept-date")
 
-casper.then(function() {
-    // click the 3rd March button for arrival
-    this.click("input[name=button1day][value='3']");
-});
+function runSteps() {
+    // start the scrapping procedure...
+    casper.start("https://secure.chenahotsprings.com/webres/webres.asp");
 
-casper.then(function() {
-    // click the submit btn
-    this.click("input.btnCal");
-});
-
-casper.waitForSelector('input.btnIDates', function() {
-    // results should be fetched by now
-    this.echo("Loaded results...");
-});
-
-casper.then(function() {
-    var noRooms = "There are no rooms available online for these dates. Please select another date or call to make a reservation.";
-    var result = this.evaluate(function() {
-        return document.querySelector("div.SelectRoomLabel").innerText;
+    // wait for the picker page to load up
+    casper.waitForSelector('div.calendarDIV', function () {
+        this.echo("Loaded page...");
     });
 
-    // echo out the results
-    if (result == noRooms) {
-        console.log("NO_ROOMS");
-    } else {
-        console.log("HAS_ROOMS");
-    }
-});
+    // update the arriving and departing month
+    casper.then(function () {
+        // go to next month for arrival
+        this.click("td.calendarmmyyyy tr td:last-child input[type=submit]");
+    });
 
-casper.run();
+    // update the arriving and departing day
+    casper.then(function () {
+        // click the designated day button for arriving
+        this.click("input[name=button1day][value='3']");
+    });
+
+    // submit the form
+    casper.then(function () {
+        // click the submit btn
+        this.click("input.btnCal");
+    });
+
+    // wait for results..
+    casper.waitForSelector('input.btnIDates', function () {
+        // results should be fetched by now
+        this.echo("Loaded results...");
+    });
+
+    casper.then(function () {
+        var noRooms = "There are no rooms available online for these dates. Please select another date or call to make a reservation.";
+        var result = this.evaluate(function () {
+            return document.querySelector("div.SelectRoomLabel").innerText;
+        });
+
+        // echo out the results
+        if (result == noRooms) {
+            console.log("NO_ROOMS");
+        } else {
+            console.log("HAS_ROOMS");
+        }
+    });
+}
+
+function reload() {
+    runSteps();
+    casper.run(function() {
+        setInterval(reload, 20000);
+    });
+}
+
+reload()
 
